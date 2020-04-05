ad02baa0393d9009391b4330d317fbf2e0a3df44
Sammed Sharad Patil <sammed.patil@yash.com>
2020-04-05 22:12:14 +0530

    hello program

---
+++ home.js
@@ -0,0 +1,1 @@
+showChangeMassege("Welcome......");
---
+++ script.js
@@ -0,0 +1,9 @@
+alert("waiting");
+
+showChangeMassege("Welcome......");
+
+
+function showChangeMassege(message)
+{
+    document.getElementById('m').textContent = message;
+}
